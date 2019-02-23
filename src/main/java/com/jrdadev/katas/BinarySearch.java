ace8ba54a025189c5ce9431f949ec68d5ad71183
josedelmartg <jose@delmartg.com>
2019-02-23 07:10:48 -0600

    chop recursively with start and end in function params

--- src/main/java/com/jrdadev/katas/BinarySearch.java
+++ src/main/java/com/jrdadev/katas/BinarySearch.java
@@ -1,8 +1,10 @@
 package com.jrdadev.katas;
 
 public class BinarySearch {
+	int[] array = null;
+	int lookingFor;
 
-	public int search(int n, int[] array) {
+	public int search1(int n, int[] array) {
 		int start = 0;
 		int end = array.length - 1;
 		int pos = (start + end) / 2; 
@@ -22,6 +24,36 @@ public class BinarySearch {
 			}
 		}
 		return -1;
+	}
+	
+	public int search(int lookingFor, int[] array) {
+		this.array = array;
+		this.lookingFor = lookingFor;
+		return recursiveSearch(0, array.length - 1);
+	}
+	
+	private int recursiveSearch(int start, int end){
+		int notFound = -1;
+		
+		if(start > end) {
+			return notFound;
+		}
+		
+		int pos = (start + end) / 2;
+		if(array[pos] == lookingFor) {
+			return pos;
+		}
+		
+		if(lookingFor > array[pos]) {
+			return recursiveSearch(pos + 1, end);
+		}
+		
+		if(lookingFor < array[pos]) {
+			return recursiveSearch(start, end - 1);
+		}
+		
+		//this code is never reached, but compiler don't think like this
+		return notFound;
 	}
 	
 }
