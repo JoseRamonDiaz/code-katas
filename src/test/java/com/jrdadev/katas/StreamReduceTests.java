f59f58322f8b90f3d1fe606dbf6ff3339c1ac7ac
josedelmartg <jose@delmartg.com>
2019-04-28 09:16:10 -0500

    fix to use TestCase

--- src/test/java/com/jrdadev/katas/StreamReduceTests.java
+++ src/test/java/com/jrdadev/katas/StreamReduceTests.java
@@ -2,37 +2,30 @@ package com.jrdadev.katas;
 
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
-import org.junit.Test;
+import junit.framework.TestCase;
 
-public class StreamReduceTests {
+public class StreamReduceTests extends TestCase {
 	private StreamReduce sr;
 	
 	protected void setUp() {
 		sr = new StreamReduce();
 	}
 	
-	@Test
 	public void testSumIntegers() throws Exception{
-		setUp();
 		assertEquals(10,  sr.sumIntegers(asList(1, 2, 3, 4)));
 	}
 	
-	@Test
-    public void transformShouldConvertCollectionElementsToUpperCase() throws Exception {
-    	setUp();
+    public void testTransformShouldConvertCollectionElementsToUpperCase() throws Exception {
         List<String> collection = asList("My", "name", "is", "John", "Doe");
         List<String> expected = asList("MY", "NAME", "IS", "JOHN", "DOE");
         List<String> transformedCollection = sr.transform(collection);
         assertThat(transformedCollection).hasSameElementsAs(expected);
     }
     
-	@Test
-    public void lessThanFourChar() throws Exception {
-    	setUp();
+    public void testLessThanFourChar() throws Exception {
     	List<String> collection = asList("My", "name", "is", "John", "Doe");
         List<String> expected = asList("My", "is", "Doe");
         assertThat(sr.filterLessThanFourChar(collection)).hasSameElementsAs(expected);
