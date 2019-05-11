65eda21d20f7329b7a39975cbb11d382a1323cd1
josedelmartg <jose@delmartg.com>
2019-05-11 08:00:24 -0500

    Add diccionary

--- src/test/java/com/codekata/_08/conflicting_objectives/ComposedWordsTest.java
+++ src/test/java/com/codekata/_08/conflicting_objectives/ComposedWordsTest.java
@@ -4,14 +4,18 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
+
+import com.codekata._04.datamunging.DataReader;
 
 public class ComposedWordsTest {
 	
 	@Test
 	public void testComposedWords() {
-		ComposedWordChecker c = new ComposedWordChecker(Arrays.asList("al", "bums", "to", "bar", "ely")) ;
+		List<String> diccionary = new DataReader("wordlist.txt").getLines();
+		ComposedWordChecker c = new ComposedWordChecker(diccionary) ;
 		assertTrue(c.isComposed("albums"));
 		assertFalse(c.isComposed("taquito"));
 		assertTrue(c.isComposed("barely"));
