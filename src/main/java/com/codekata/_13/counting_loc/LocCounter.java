1fb565ed936144ea385eee94e939f9bde4d4602d
ramon <dajoseramon@gmail.com>
2019-07-08 15:59:28 -0500

    better multi line comment count

--- src/main/java/com/codekata/_13/counting_loc/LocCounter.java
+++ src/main/java/com/codekata/_13/counting_loc/LocCounter.java
@@ -5,23 +5,17 @@ import com.codekata._04.datamunging.DataReader;
 import java.util.List;
 
 public class LocCounter {
+    int emptyLines = 0;
+    boolean multilineComment = false;
+    LocMatcher locMatcher = new LocMatcher();
 
     public int getCount(String fileName){
         DataReader dataReader = new DataReader(fileName);
         List<String> lines = dataReader.getLines();
-        LocMatcher locMatcher = new LocMatcher();
-        int emptyLines = 0;
-        boolean multilineComment = false;
 
         for(String line : lines){
             if(multilineComment){
-                if(locMatcher.isMultilineCommentEnd(line)){
-                    emptyLines++;
-                    multilineComment = false;
-                }else{
-                    emptyLines++;
-                    continue;
-                }
+                countMultilineCommentLines(line);
             }
 
             if(locMatcher.isSingleLineComment(line) || locMatcher.isMultilineCommentInLine(line)) {
@@ -35,5 +29,14 @@ public class LocCounter {
             }
         }
         return lines.size() - emptyLines;
+    }
+
+    private void countMultilineCommentLines(String line) {
+        if(locMatcher.isMultilineCommentEnd(line)){
+            emptyLines++;
+            multilineComment = false;
+        }else{
+            emptyLines++;
+        }
     }
 }
