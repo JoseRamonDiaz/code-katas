696d097a065fe934aebb9aab02845fd5b202184d
ramon <dajoseramon@gmail.com>
2019-08-24 07:29:26 -0500

    Transitive dependencies

--- src/main/java/com/codekata/_18/transitive_dependencies/TransitiveDependencies.java
+++ src/main/java/com/codekata/_18/transitive_dependencies/TransitiveDependencies.java
@@ -1,16 +1,24 @@
 package com.codekata._18.transitive_dependencies;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class TransitiveDependencies {
     Map<String, String[]> map = new HashMap<>();
+    Set<String> dependenciesSet = new HashSet<>();
 
     public void addDirect(String c, String[] strings) {
         map.put(c, strings);
     }
 
     public String[] getDependencies(String c) {
-        return map.get(c);
+        if(map.get(c) == null){
+            return null;
+        }
+
+        for(String dependency : map.get(c)){
+            getDependencies(dependency);
+        }
+
+        return null;
     }
 }
