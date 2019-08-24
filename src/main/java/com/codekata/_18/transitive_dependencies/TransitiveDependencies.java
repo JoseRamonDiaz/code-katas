4be30cec550e5743441f447a86b328bf30e74179
ramon <dajoseramon@gmail.com>
2019-08-24 08:19:06 -0500

    Change String array by String set

--- src/main/java/com/codekata/_18/transitive_dependencies/TransitiveDependencies.java
+++ src/main/java/com/codekata/_18/transitive_dependencies/TransitiveDependencies.java
@@ -5,20 +5,25 @@ import java.util.*;
 public class TransitiveDependencies {
     Map<String, String[]> map = new HashMap<>();
     Set<String> dependenciesSet = new HashSet<>();
+    Set<String> usedKeys = new HashSet<>();
 
     public void addDirect(String c, String[] strings) {
         map.put(c, strings);
     }
 
-    public String[] getDependencies(String c) {
-        if(map.get(c) == null){
-            return null;
+    public Set<String> getDependencies(String c){
+        String[] tempArray = map.get(c);
+        if((tempArray != null) && (!usedKeys.contains(c))){
+            usedKeys.add(c);
+            for(String s : tempArray){
+                dependenciesSet.add(s);
+            }
+
+            for(String s : tempArray){
+                getDependencies(s);
+            }
         }
 
-        for(String dependency : map.get(c)){
-            getDependencies(dependency);
-        }
-
-        return null;
+        return dependenciesSet;
     }
 }
--- src/test/java/com/codekata/_18/transitive_dependencies/TransitiveDependenciesTest.java
+++ src/test/java/com/codekata/_18/transitive_dependencies/TransitiveDependenciesTest.java
@@ -2,6 +2,9 @@ package com.codekata._18.transitive_dependencies;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 import static org.junit.Assert.assertEquals;
 
 
@@ -11,7 +14,7 @@ public class TransitiveDependenciesTest {
     public void testOneDependency(){
         TransitiveDependencies transitiveDependencies = new TransitiveDependencies();
         transitiveDependencies.addDirect("c", new String[] {"g"});
-        assertEquals(new String[] {"g"}, transitiveDependencies.getDependencies("c"));
+        assertEquals(new HashSet<>(Arrays.asList("g")), transitiveDependencies.getDependencies("c"));
     }
 
     @Test
@@ -19,6 +22,7 @@ public class TransitiveDependenciesTest {
         TransitiveDependencies transitiveDependencies = new TransitiveDependencies();
         transitiveDependencies.addDirect("e", new String[] {"f"});
         transitiveDependencies.addDirect("f", new String[] {"h"});
-        assertEquals(new String[] {"f", "h"}, transitiveDependencies.getDependencies("e"));
+        transitiveDependencies.addDirect("h", new String[] {"e"});
+        assertEquals(new HashSet<>(Arrays.asList("f", "h", "e")) , transitiveDependencies.getDependencies("e"));
     }
 }
