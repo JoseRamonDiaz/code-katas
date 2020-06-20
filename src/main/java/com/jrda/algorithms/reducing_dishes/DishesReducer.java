9e0cffb1ac7d249781df48ea3b110c49a189a2a8
ramon <dajoseramon@gmail.com>
2020-06-20 08:00:30 -0500

    Fastest implementation on leetcode :D

--- src/main/java/com/jrda/algorithms/reducing_dishes/DishesReducer.java
+++ src/main/java/com/jrda/algorithms/reducing_dishes/DishesReducer.java
@@ -1,105 +1,39 @@
 package com.jrda.algorithms.reducing_dishes;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Stack;
 
 public class DishesReducer {
     public int maxSatisfaction(int[] satisfaction) {
         Arrays.sort(satisfaction);
+        Integer negativePointer = null;
+        int i = 0, accum = 0, baseSum = 0;
+        for (int pos = 0; pos < satisfaction.length; pos++) {
+            if (satisfaction[pos] >= 0) {
+                if(negativePointer == null) {
+                    negativePointer = pos - 1;
+                }
 
-        Stack<Integer> negativeSatisfaction = getNegativeSatisfactionsStack(satisfaction);
-        List<Integer> nonNegativeSatisfaction = getNonNegativeSatisfactions(satisfaction);
-        int baseSum = getSum(nonNegativeSatisfaction);
-        int lastVal = getBaseProduct(nonNegativeSatisfaction);
+                i++;
+                accum += i * satisfaction[pos];
+
+                baseSum += satisfaction[pos];
+            }
+        }
+
+        if (negativePointer == null || negativePointer < 0) {
+            return accum;
+        }
 
         do {
-            if (negativeSatisfaction.isEmpty()) {
-                return lastVal;
-            }
-
-            //add the last negative satisfaction
-            baseSum += negativeSatisfaction.pop();
-
-            //if the number is positive, the count should increase with that value. lastVal = newVal
+            baseSum += satisfaction[negativePointer];
             if (baseSum > 0) {
-                lastVal += baseSum;
+                accum += baseSum;
             } else {
-                return lastVal;
+                return accum;
             }
-        } while (! negativeSatisfaction.isEmpty());
+            negativePointer--;
+        } while (negativePointer >= 0);
 
-        return lastVal;
-    }
-
-    /**
-     *
-     * @param satisfaction needs to be a sorted array of int
-     * @return list of non negative values
-     */
-    protected List<Integer> getNegativeSatisfactions(int[] satisfaction) {
-        List<Integer> negativeSatisfaction = new ArrayList<>();
-
-        for (int value : satisfaction) {
-            if (value < 0) {
-                negativeSatisfaction.add(value);
-            } else {
-                break;
-            }
-        }
-
-        return negativeSatisfaction;
-    }
-
-    public Stack<Integer> getNegativeSatisfactionsStack(int[] satisfaction) {
-        Stack<Integer> result = new Stack<>();
-
-        for (int value : satisfaction) {
-            if (value < 0) {
-                result.push(value);
-            } else {
-                break;
-            }
-        }
-
-        return result;
-    }
-
-    /**
-     * Maybe we should sort the array inside the method and avoid future problems
-     * @param satisfaction needs to be a sorted array of int in order to return the correct value
-     * @return subset of non-negative values
-     */
-    public List<Integer> getNonNegativeSatisfactions(int[] satisfaction) {
-        List<Integer> nonNegativeSatisfaction = new ArrayList<>();
-
-        for (int value : satisfaction) {
-            if (value >= 0) {
-                nonNegativeSatisfaction.add(value);
-            }
-        }
-
-        return nonNegativeSatisfaction;
-    }
-
-    protected int getBaseProduct(List<Integer> nonNegativeSatisfaction) {
-        int baseProduct = 0;
-
-        for(int i = 0; i < nonNegativeSatisfaction.size(); i++) {
-            baseProduct += nonNegativeSatisfaction.get(i) * (i+1);
-        }
-
-        return baseProduct;
-    }
-
-    protected int getSum(List<Integer> nonNegativeSatisfaction) {
-        int sum = 0;
-
-        for (Integer num : nonNegativeSatisfaction) {
-            sum += num;
-        }
-
-        return sum;
+        return accum;
     }
 }
--- src/test/java/com/jrda/algorithms/reducing_dishes/DishesReducerTest.java
+++ src/test/java/com/jrda/algorithms/reducing_dishes/DishesReducerTest.java
@@ -3,10 +3,6 @@ package com.jrda.algorithms.reducing_dishes;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Stack;
-
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class DishesReducerTest {
@@ -26,60 +22,5 @@ public class DishesReducerTest {
         assertEquals(20, dishesReducer.maxSatisfaction(new int[]{4,3,2}));
         assertEquals(0, dishesReducer.maxSatisfaction(new int[]{-1,-4,-5}));
         assertEquals(35, dishesReducer.maxSatisfaction(new int[]{-2,5,-1,0,3,-3}));
-    }
-
-    @Test
-    public void testSum() {
-        assertEquals(0, dishesReducer.getSum(Arrays.asList(0)));
-        assertEquals(1, dishesReducer.getSum(Arrays.asList(0, 1)));
-        assertEquals(3, dishesReducer.getSum(Arrays.asList(0, 1, 2)));
-        assertEquals(6, dishesReducer.getSum(Arrays.asList(1, 2, 3)));
-    }
-
-    @Test
-    public void testGetBaseProduct() {
-        assertEquals(10, dishesReducer.getBaseProduct(Arrays.asList(0, 5)));
-        assertEquals(17, dishesReducer.getBaseProduct(Arrays.asList(0, 1, 5)));
-        assertEquals(11, dishesReducer.getBaseProduct(Arrays.asList(1, 5)));
-        assertEquals(-5, dishesReducer.getBaseProduct(Arrays.asList(-1, -2)));
-        assertEquals(-8, dishesReducer.getBaseProduct(Arrays.asList(-2, -3)));
-    }
-
-    @Test
-    public void testNegativeSatisfactions () {
-        int[] ints = new int[]{-1,-8,0,5,-9};
-        Arrays.sort(ints);
-        assertEquals(Arrays.asList(-9, -8, -1), dishesReducer.getNegativeSatisfactions(ints));
-    }
-
-    @Test
-    public void testGetNegativeSatisfactionsStack() {
-        int[] ints = new int[]{-1,-8,0,5,-9};
-        Arrays.sort(ints);
-        Stack<Integer> integerStack = new Stack<>();
-        integerStack.push(-9);
-        integerStack.push(-8);
-        integerStack.push(-1);
-        assertEquals(integerStack, dishesReducer.getNegativeSatisfactionsStack(ints));
-    }
-
-    @Test
-    public void testNonNegativeSatisfactions() {
-        int[] ints = new int[]{-1,-8,0,5,-9};
-        Arrays.sort(ints);
-        assertEquals(Arrays.asList(0, 5), dishesReducer.getNonNegativeSatisfactions(ints));
-    }
-
-    @Test
-    public void testAscendingSort() {
-        int[] ints = new int[]{-1,-8,0,5,-9};
-        Arrays.sort(ints);
-        assertArrayEquals(new int[]{-9, -8, -1, 0, 5}, ints);
-    }
-
-    @Test
-    public void testNegativeSubListSum() {
-        assertEquals(-1, dishesReducer.getBaseProduct(Arrays.asList(-3, -2, -1).subList(2, 3)));
-        assertEquals(-4, dishesReducer.getBaseProduct(Arrays.asList(-3, -2, -1).subList(1, 3)));
     }
 }
