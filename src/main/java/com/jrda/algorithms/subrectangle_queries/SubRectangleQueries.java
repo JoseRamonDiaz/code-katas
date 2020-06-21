73c3d8c12bb2d2e42ed162c024ec902f0ca5bb3e
ramon <dajoseramon@gmail.com>
2020-06-21 07:31:32 -0500

    Solve subrectangle queries

---
+++ src/main/java/com/jrda/algorithms/subrectangle_queries/SubRectangleQueries.java
@@ -0,0 +1,21 @@
+package com.jrda.algorithms.subrectangle_queries;
+
+public class SubRectangleQueries {
+    private int[][] rectangle;
+
+    public SubRectangleQueries(int[][] rectangle) {
+        this.rectangle = rectangle;
+    }
+
+    public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+        for (int i = row1; i <= row2; i++) {
+            for (int j = col1; j <= col2; j++) {
+                rectangle[i][j] = newValue;
+            }
+        }
+    }
+
+    public int getValue(int row, int col) {
+        return rectangle[row-1][col-1];
+    }
+}
