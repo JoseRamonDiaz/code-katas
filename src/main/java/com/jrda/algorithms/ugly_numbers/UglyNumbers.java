9062e4427f80f8fc271b4c233e51231671570c57
Ramon <jose.diaz@verato.com>
2020-07-05 13:16:16 -0500

    Solve ugly numbers

--- src/main/java/com/jrda/algorithms/ugly_numbers/UglyNumbers.java
+++ src/main/java/com/jrda/algorithms/ugly_numbers/UglyNumbers.java
@@ -4,27 +4,41 @@ package com.jrda.algorithms.ugly_numbers;
  * Problem https://leetcode.com/problems/ugly-number-ii/
  */
 public class UglyNumbers {
-    private boolean[] primas = new boolean[5000];
-    private int lastNumberUsed = 1;
+   public int nthUglyNumber(int n) {
+       int[] uglyNum = new int[n];
+       int i2 = 0, i3 = 0, i5 = 0;
 
-    public boolean[] calculatePrimeNumbers(int n) {
-        if (n == 0 || n == 1 || n == lastNumberUsed) {
-            return primas;
-        }
+       int next2mul = 2, next3mul = 3, next5mul = 5;
+       int next = 1;
+       uglyNum[0] = 1;
 
-        //calculate the next number
-        lastNumberUsed++;
+       for (int i = 1; i < n; i++) {
+           next = min(next2mul, next3mul, next5mul);
+           uglyNum[i] = next;
+           if (next == next2mul){
+               i2++;
+               next2mul = uglyNum[i2] * 2;
+           }
 
-        for (int i = lastNumberUsed; i <= n; i++) {
-            int j = 2;
-            int currentValue = i * j;
-            while (currentValue < 5000) {
-                primas[currentValue] = true;
-                j++;
-                currentValue = i * j;
-            }
-        }
-        lastNumberUsed = n;
-        return primas;
-    }
+           if (next == next3mul) {
+               i3++;
+               next3mul = uglyNum[i3] * 3;
+           }
+
+           if (next == next5mul) {
+               i5++;
+               next5mul = uglyNum[i5] * 5;
+           }
+       }
+
+       return next;
+   }
+
+   protected int min(int a, int b, int c) {
+       if (a < b) {
+           return Math.min(a, c);
+       } else {
+           return Math.min(b, c);
+       }
+   }
 }
--- src/test/java/com/jrda/algorithms/ugly_numbers/UglyNumbersTest.java
+++ src/test/java/com/jrda/algorithms/ugly_numbers/UglyNumbersTest.java
@@ -2,18 +2,31 @@ package com.jrda.algorithms.ugly_numbers;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class UglyNumbersTest {
     @Test
-    public void testGetNPrimeNumbers() {
+    public void tesUglies() {
         UglyNumbers uglyNumbers = new UglyNumbers();
-        boolean[] expectedResponse = new boolean[5000];
+        assertEquals(1, uglyNumbers.nthUglyNumber(1));
+        assertEquals(2, uglyNumbers.nthUglyNumber(2));
+        assertEquals(3, uglyNumbers.nthUglyNumber(3));
+        assertEquals(4, uglyNumbers.nthUglyNumber(4));
+        assertEquals(5, uglyNumbers.nthUglyNumber(5));
+        assertEquals(6, uglyNumbers.nthUglyNumber(6));
+        assertEquals(8, uglyNumbers.nthUglyNumber(7));
+        assertEquals(9, uglyNumbers.nthUglyNumber(8));
+        assertEquals(10, uglyNumbers.nthUglyNumber(9));
+        assertEquals(12, uglyNumbers.nthUglyNumber(10));
+        assertEquals(15, uglyNumbers.nthUglyNumber(11));
+        assertEquals(64, uglyNumbers.nthUglyNumber(27));
+    }
 
-        assertArrayEquals(expectedResponse, uglyNumbers.calculatePrimeNumbers(1));
-        assertTrue(uglyNumbers.calculatePrimeNumbers(2)[4]);
-        assertTrue(uglyNumbers.calculatePrimeNumbers(2)[6]);
-        assertFalse(uglyNumbers.calculatePrimeNumbers(2)[2]);
-
+    @Test
+    public void testMin() {
+        UglyNumbers uglyNumbers = new UglyNumbers();
+        assertEquals(1, uglyNumbers.min(1, 2, 3));
+        assertEquals(1, uglyNumbers.min(2, 1, 3));
+        assertEquals(1, uglyNumbers.min(3, 2, 1));
     }
 }
