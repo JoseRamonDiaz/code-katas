258f4cce971d467ec8b56ac0d623f9deb025098d
ramon <dajoseramon@gmail.com>
2021-11-05 21:44:18 -0600

    Fix node iterator

--- src/main/java/com/jrda/algorithms/inheritance/NodeIterator.java
+++ src/main/java/com/jrda/algorithms/inheritance/NodeIterator.java
@@ -1,47 +1,33 @@
 package com.jrda.algorithms.inheritance;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 public class NodeIterator implements Iterator<Node> {
-    private final Map<Node, Integer> childIndexMap = new HashMap<>();
-    private Node current;
+    private List<Node> nodeList;
+    private int position = 0;
 
     public NodeIterator(Node current) {
-        this.current = current;
+       nodeList = new ArrayList<>();
+       flatMap(current);
+    }
+
+    private void flatMap(Node current) {
+        nodeList.add(current);
+
+        if (current.getChildren() != null && !current.getChildren().isEmpty()) {
+            for (Node n : current.getChildren()) {
+                flatMap(n);
+            }
+        }
     }
 
     @Override
     public boolean hasNext() {
-        return current != null;
+        return nodeList.size() > position;
     }
 
     @Override
     public Node next() {
-        //TODO create a flat list on iterator creation, to make it easy to traverse and avoid problems preserving the state
-        childIndexMap.putIfAbsent(current, -1);
-
-        Node next = current;
-
-        childIndexMap.put(current, childIndexMap.get(current) + 1);
-
-        if (current.getChildren() == null || current.getChildren().isEmpty() || childIndexMap.get(current) >= current.getChildren().size()) {
-            //parent != null && parentChildrenIndex < parent.children.length
-            //current = parent.getChild
-            while (current.getParent() != null && childIndexMap.get(current.getParent()) >= current.getParent().getChildren().size()) {
-                current = current.getParent();
-            }
-
-            if (current != null && childIndexMap.get(current) < current.getChildren().size()) {
-                current = current.getChildren().get(childIndexMap.get(current));
-            } else {
-                current = null;
-            }
-
-        } else
-            current = current.getChildren().get(childIndexMap.get(current));
-
-        return next;
+        return nodeList.get(position++);
     }
 }
