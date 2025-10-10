4b27a87a692369d1d85cd9d08ba6be374a5a9aca
Ramon <tu_correo@example.com>
2025-10-09 22:12:59 -0600

    nqnn

---
+++ src/main/java/com/jrda/algorithms/n_queens/NQNN.java
@@ -0,0 +1,43 @@
+package com.jrda.algorithms.n_queens;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NQNN {
+  private int[] queens;
+  private List<int[]> solutions;
+  private int n;
+
+  public void solve(int n) {
+    queens = new int[n];
+    solutions = new ArrayList<>();
+    this.n = n;
+
+    placeQueens(0);
+    System.out.println("Solutions: " + solutions.size());
+  }
+
+  private void placeQueens(int row) {
+    if (row == n)
+      solutions.add(queens.clone());
+
+    for (int col = 0; col < n; col++) {
+      if (isSafe(row, col)) {
+        queens[row] = col;
+        placeQueens(row + 1);
+      }
+    }
+  }
+
+  private boolean isSafe(int row, int col) {
+    for (int prevRow = 0; prevRow < row; prevRow++) {
+      int prevCol = queens[prevRow];
+      if (prevCol == col)
+        return false;
+      if (Math.abs(prevRow - row) == Math.abs(prevCol - col))
+        return false;
+    }
+    return true;
+  }
+
+}
--- src/test/java/com/jrda/algorithms/n_queens/NQueensBacktrackingTest.java
+++ src/test/java/com/jrda/algorithms/n_queens/NQueensBacktrackingTest.java
@@ -3,21 +3,27 @@ package com.jrda.algorithms.n_queens;
 import org.junit.Test;
 
 public class NQueensBacktrackingTest {
-	@Test
-	public void test() {
-		NQueensBacktracking nq = new NQueensBacktracking();
-		nq.solve(8);
-	}
-	
-	@Test
-	public void testNQ() {
-		NQ nq = new NQ();
-		nq.solve(8);
-	}
-	
-	@Test
-	public void testNQN() {
-		NQN nq = new NQN();
-		nq.solve(8);
-	}
+  @Test
+  public void test() {
+    NQueensBacktracking nq = new NQueensBacktracking();
+    nq.solve(8);
+  }
+
+  @Test
+  public void testNQ() {
+    NQ nq = new NQ();
+    nq.solve(8);
+  }
+
+  @Test
+  public void testNQN() {
+    NQN nq = new NQN();
+    nq.solve(8);
+  }
+
+  @Test
+  public void testNQNN() {
+    NQNN nq = new NQNN();
+    nq.solve(8);
+  }
 }
